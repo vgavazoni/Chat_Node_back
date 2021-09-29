dcfc7a3eb234bd4b9bd87d00904592ba99efa8f7
gavaza <vinicius.gavazoni@gmail.com>
2021-09-28 22:24:50 -0300

    Subindo o back

---
+++ README.md
@@ -0,0 +1,39 @@
+Seuqencia para criar o arquivo
+
+Criar o arquivo package
+### npm init
+
+Gerenciar as requisições, rotas, entre outras funcionalidades
+### npm install express
+
+Start
+### node app.js
+
+Instalar globalmente quem não tem o nodemon
+### npm install -g nodemon
+
+Após a anterior rodar essa
+### npm install --save-dev nodemon
+
+Agora para startar é só usar
+### nodemon app.js
+
+Socket.IO is a library that enables real-time, bidirectional and event-based communication between the browser and the server.
+### npm install socket.io
+
+Permitir acesso a API
+### npm install cors
+
+Instalar o React que é front fora da pasta backend, uma pasta anterior que vai ser o front
+### npx create-react-app front-end
+
+Dentro da pasta front-end eu rodo
+### npm start
+
+Precisamos instalar o socket IO na versão do cliente ou seja no fron-end
+rodar o comando abaixo na pasta do front
+### npm install socket.io-client
+
+
+
+
---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const socket = require('socket.io');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");  // Endereço permitido qqum
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+    app.use(cors()); // se não der pau
+    next();  // próximo
+})
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World ehehe')
+})
+ 
+const server = app.listen(8080, ()=> {
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080")
+});
+
+io = socket(server, {cors: {origin: "*"}});
+
+io.on("connection", (socket) => {  // Para cada user eu to gerando uma ID
+    console.log(socket.id);
+
+    socket.on("sala_conectar", (dados) => {
+        console.log("Sala selecionada: " + dados);
+        socket.join(dados); // fazendo a conexão da mesma sala com o front
+    });
+
+    socket.on("enviar_mensagem", (dados) => {
+        console.log(dados);
+        socket.to(dados.sala).emit("receber_mensagem", dados.conteudo); // isso é um broadcast, todos da sala devem receber a msg
+    });
+});
